00a686f36bbabdee7491d8d7ae964974c0f2285a
Rahul Kumar <rahulrai2822march@gmail.com>
2024-04-26 12:47:18 +0530

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
-const initialGameBoard = [
+
+
+//GENERAL CONSTANTS
+const PLAYERS = {
+  X: "PLAYER 1",
+  O: "PLAYER 2",
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-//HELPER FUNCTION
-
+//HELPER FUNCTIONS
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
@@ -21,22 +28,21 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({ 'X': "PLAYER 1", 'O': "PLAYER 2" });
-  //ARRAY OF OBJECTS TURNS AND CORRESPONDING PLAYER INFOR
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState("X");
-  const activePlayer = deriveActivePlayer(gameTurns);
-
+function deriveGameBoard(gameTurns) {
   //DEEP CLONING OF 2D ARRAY/OBJECTS
-  const gameBoard = [...initialGameBoard.map((array) => [...array])];
-  console.log(initialGameBoard);
+  const gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
+  console.log(INITIAL_GAME_BOARD);
   // DERIVING STATE
   for (const turn of gameTurns) {
     const { square, player } = turn;
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
+
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
   let winner;
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
@@ -55,7 +61,21 @@ function App() {
     }
   }
 
+  return winner;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  //ARRAY OF OBJECTS TURNS AND CORRESPONDING PLAYER INFOR
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, players);
   const isDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectActivePlayer(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
 
@@ -89,13 +109,13 @@ function App() {
           <Player
             name="Player 1"
             symbol="X"
-            isActive={activePlayer === "X"}
+            isActive={activePlayer === PLAYERS.X}
             handlePlayersNameChange={handlePlayersNameChange}
           />
           <Player
             name="Player 2"
             symbol="O"
-            isActive={activePlayer === "O"}
+            isActive={activePlayer === PLAYERS.O}
             handlePlayersNameChange={handlePlayersNameChange}
           />
         </ol>
